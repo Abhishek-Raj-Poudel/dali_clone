19e11074f5f1c4a58ee6196084e967fa065a8b3e
chronoabi <abishekpoudel2014@gmail.com>
2023-03-16 19:37:25 +0545

    Initial setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { logo } from "./assets";
+export default function App() {
+  return <h1 className="text-3xl font-bold ">Hello Mothore</h1>;
+}
