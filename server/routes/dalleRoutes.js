7f4233e4ca9ff5e44aae35d9fc871e446d8fe232
chronoabi <abishekpoudel2014@gmail.com>
2023-03-17 00:37:41 +0545

    env removed

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -7,7 +7,7 @@ dotenv.config();
 const router = express.Router();
 
 const configuration = new Configuration({
-  apiKey: process.env.OPEN_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -19,6 +19,7 @@ router.route("/").get((req, res) => {
 router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
+
     const aiResponse = await openai.createImage({
       prompt,
       n: 1,
@@ -26,8 +27,9 @@ router.route("/").post(async (req, res) => {
       response_format: "b64_json",
     });
 
-    const image = aiResponse.data.data[0].b64_json;
-    res.status(200).json({ photo: image });
+    const image = aiResponse;
+    console.log(image);
+    // res.status(200).json({ photo: image });
   } catch (error) {
     console.log(error);
     res.status(500).send(error?.response.data.error.message);
